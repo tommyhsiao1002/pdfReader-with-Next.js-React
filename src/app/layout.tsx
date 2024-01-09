8c5b5ad6a5fc9f8104f00cdaa59b3a4aaaace004
tommyhsiao1002 <79104293+tommyhsiao1002@users.noreply.github.com>
2024-01-09 10:30:38 -0600

    Finish the file uploader and the pdf renderer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Navbar from "../components/Navbar";
 import Providers from "@/components/Providers";
 
 import "react-loading-skeleton/dist/skeleton.css"
+import "simplebar-react/dist/simplebar.min.css"
+import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,6 +29,7 @@ export default function RootLayout({
           inter.className
         )}
       >
+        <Toaster />
         <Navbar />
         {children}
       </body>
